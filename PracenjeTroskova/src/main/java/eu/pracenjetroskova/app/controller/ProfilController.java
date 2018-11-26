3e31d453d9b5e5890afb8ff7440b1175e087b800
Branimir Pehar <bp119123786@fer.hr>
2018-11-26 19:43:28 +0100

    Dodane TODO metode u profil controller

--- PracenjeTroskova/src/main/java/eu/pracenjetroskova/app/controller/ProfilController.java
+++ PracenjeTroskova/src/main/java/eu/pracenjetroskova/app/controller/ProfilController.java
@@ -3,6 +3,7 @@ package eu.pracenjetroskova.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfilController {
@@ -14,6 +15,51 @@ public class ProfilController {
 		return "home";
 	}
 	
+	@GetMapping("/profil/troskovi")
+	public String pregledTroskova() {
+		//TODO
+		return "troskovi";
+	}
 	
+	@GetMapping("/profil/prihodi")
+	public String pregledPrihoda() {
+		//TODO
+		return "prihodi";
+	}
+	
+	@GetMapping("/profil/stednje")
+	public String pregledStednji() {
+		//TODO
+		return "stednje";
+	}
+	
+	@GetMapping("/profil/stednje/zajednicke")
+	public String pregledZajednickihStednji() {
+		//TODO
+		return "zajednickeStednje";
+	}
 
+	@PostMapping("/profil/troskovi")
+	public void stvoriNoviTrosak() {
+		//TODO
+		
+	}
+	
+	@PostMapping("/profil/prihodi")
+	public void stvoriNoviPrihod() {
+		//TODO
+		
+	}
+
+	@PostMapping("/profil/stednje")
+	public void stvoriNovuStednju() {
+		//TODO
+		
+	}
+	
+	@PostMapping("/profil/stednje/zajednicke")
+	public void stvoriNovuZajednickuStednju() {
+		//TODO
+		
+	}
 }
